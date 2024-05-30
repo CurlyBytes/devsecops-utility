3d7890970353b85c11b8b49c350fd777b6ac0211
Francisco Abayon <francisco.abayon@rafi.ph>
2024-05-30 08:40:55 +0800

    3

--- src/infrastructure-as-code/terraform/samples/az-standard-rg/main.tf
+++ src/infrastructure-as-code/terraform/samples/az-standard-rg/main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 }
 
 module "aztf_rnd_rg" {
-  source        = "../../terraform/components/azure-resource-group"
+  source        = "../../../terraform/components/azure-resource-group"
   # source                   = "../../components/azure-resource-group"
   PROJECT_NAME             = "devsecops-utility"
   PROJECT_ENVIRONMENT_NAME = "Sandbox"
